9ab982d053f7e982d2eee39a0d7e9df281281fbf
jimmy <jimmmmystudio@163.com>
2023-10-07 16:19:38 +0800

    Spring annotated aop

--- spring-annotated-aop/src/main/java/com/maguasoft/spring/aop/LoggerAdvice.java
+++ spring-annotated-aop/src/main/java/com/maguasoft/spring/aop/LoggerAdvice.java
@@ -9,7 +9,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggerAdvice {
 
-    @Before("execution(* com.maguasoft.spring..* (..))")
+    /**
+     * 定义公共的切入点，
+     * 在@Before/@AfterReturning/@AfterThrowing/@Around/@After时，可以使用<类名.方法名>引用切入点。e.g：LoggerAdvice.pointcut()
+     */
+    @Pointcut("execution(* com.maguasoft.spring..* (..))")
+    public void pointcut() {
+
+    }
+
+    @Before("LoggerAdvice.pointcut()") // 引用定义好的切入点，语法：<类名.方法名>
+//    @Before("execution(* com.maguasoft.spring..* (..))")
     public void before(JoinPoint joinPoint) {
         System.out.println("LoggerAdvice.before");
         System.out.printf("LoggerAdvice.before, %s-%s \n", joinPoint.getTarget(), joinPoint.getSignature());
