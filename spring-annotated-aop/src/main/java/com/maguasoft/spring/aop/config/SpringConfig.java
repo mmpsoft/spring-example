b786d9e55371070b6237c4263b5ab2f21cc244ab
jimmy <jimmmmystudio@163.com>
2023-10-07 16:11:21 +0800

    Spring annotated aop

--- spring-annotated-aop/src/main/java/com/maguasoft/spring/aop/config/SpringConfig.java
+++ spring-annotated-aop/src/main/java/com/maguasoft/spring/aop/config/SpringConfig.java
@@ -3,7 +3,7 @@ package com.maguasoft.spring.aop.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
-@ComponentScan("com.maguasoft.spring.aop")
+@EnableAspectJAutoProxy // 开启注解配置aop
+@ComponentScan("com.maguasoft.spring.aop") // 包扫描
 public class SpringConfig {
 }
